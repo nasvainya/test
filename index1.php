be74fbf5671243c76d46c4a9bff3a8d84d39b389
Ваше Имя <you@example.com>
2026-03-19 10:01:41 +0300

    Реализована валидация формы

---
+++ index1.php
@@ -0,0 +1,20 @@
+ <?php 
+    <form action="action.php" method="post">
+<div>
+        <p>Имя: <input type="text" name="name"/></p>
+</div>
+        <p>Почта: <input type="email" email="email"/></p>
+        <p>Пароль: <input type="text" password="password"/></p>
+        <p>Подтвердите пароль: <input type="text" checkpswd="checkpswd"/></p>
+        <label>Пол</label>
+            <label>
+            <input type="radio" name="gender">
+                Мужчина
+            </label>
+            <label>
+            <input type="radio" name="gender">
+                Женщина
+            </label>
+        <button>
+    </form>
+?>
